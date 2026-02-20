34294c3b189ffc229c4a0a67c76d061a6189c2f7
Umair Akbar <umairakbar5665@gmail.com>
2026-02-20 23:50:19 +0500

    Added homescreen

---
+++ lib/homescreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _homescreenState();
+}
+
+class _homescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'homescreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return GetMaterialApp(home: Homescreen());
   }
 }
